cf5e927dcbc36eaecb1af8feb931c1b5e1819eda
maaznisar <syedmaaznisar@gmail.com>
2018-05-29 17:04:29 +0500

    Add xUnit test project

---
+++ SamplifyAPIClientTest/MockSamplifyClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using ResearchNow.SamplifyAPIClient;
+
+namespace SamplifyAPIClientTest
+{
+    public class MockSamplifyClient : SamplifyClient
+    {
+        public MockSamplifyClient(Action<HttpRequestMessage> onRequestReceived)
+            : base(new HttpClient(new MockHandler(onRequestReceived)))
+        {
+            var auth = new TokenResponse();
+            auth.AccessToken = "test-token";
+            auth.Acquired = DateTime.Now;
+            auth.ExpiresIn = 3600;
+            base.Auth = auth;
+        }
+
+        private class MockHandler : HttpMessageHandler
+        {
+            private Action<HttpRequestMessage> Callback;
+
+            public MockHandler(Action<HttpRequestMessage> onRequestReceived)
+            {
+                this.Callback = onRequestReceived;
+            }
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                this.Callback(request);
+                var res = new HttpResponseMessage(System.Net.HttpStatusCode.NotImplemented);
+                res.Content = new StringContent("{}", System.Text.Encoding.UTF8, "application/json");
+                return await Task.Run(() => res).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ SamplifyAPIClientTest/SamplifyClientTest.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using ResearchNow.SamplifyAPIClient;
+using Xunit;
+
+namespace SamplifyAPIClientTest
+{
+    public class SamplifyClientTest
+    {
+        [Fact]
+        public void TestAuth()
+        {
+            const string testToken = "test-token";
+            const string expectedToken = "Bearer test-token";
+            HttpRequestMessage message = new HttpRequestMessage();
+            SamplifyClient testClient = new MockSamplifyClient((request) =>
+            {
+                message = request;
+            });
+            testClient.Auth.AccessToken = testToken;
+            testClient.GetAllProjects(null).Wait();
+            Assert.NotNull(message);
+            Assert.NotNull(message.Headers);
+            Assert.NotNull(message.Headers.Authorization);
+            Assert.Equal(message.Headers.Authorization.ToString(), expectedToken);
+        }
+
+        [Fact]
+        public void TestEndpoints()
+        {
+            var messages = new List<HttpRequestMessage>();
+            string[] tests = new string[]{
+                "/projects",
+                "/projects/update-test",
+                "/projects/buy-test/buy",
+                "/projects/close-test/close",
+                "/projects",
+                "/projects/test-prj-id",
+                "/projects/test-report-id/report",
+                "/projects/test/lineItems",
+                "/projects/test-prj-id/lineItems/test-lineitem-id",
+                "/projects/test-prj-id/lineItems/test-lineitem-id/pause",
+                "/projects/test-prj-id/lineItems",
+                "/projects/test-prj-id/lineItems/test-lineitem-id",
+                "/projects/test-prj-id/feasibility",
+                "/countries",
+                "/attributes/GB/en",
+                "/categories/surveyTopics"};
+
+            SamplifyClient testClient = new MockSamplifyClient((request) =>
+            {
+                messages.Add(request);
+            });
+
+            var project = new CreateUpdateProjectCriteria();
+            project.ExtProjectID = "update-test";
+            var lineItem = new LineItemCriteria();
+            lineItem.ExtLineItemID = "test-lineitem-id";
+
+            testClient.CreateProject(project).Wait();
+            testClient.UpdateProject(project).Wait();
+            testClient.BuyProject("buy-test", null).Wait();
+            testClient.CloseProject("close-test").Wait();
+            testClient.GetAllProjects(null).Wait();
+            testClient.GetProjectBy("test-prj-id").Wait();
+            testClient.GetProjectReport("test-report-id").Wait();
+            testClient.AddLineItem("test", lineItem).Wait();
+            testClient.UpdateLineItem("test-prj-id", "test-lineitem-id", lineItem).Wait();
+            testClient.UpdateLineItemState("test-prj-id", "test-lineitem-id", ActionConstants.ActionPaused).Wait();
+            testClient.GetAllLineItems("test-prj-id", null).Wait();
+            testClient.GetLineItemBy("test-prj-id", "test-lineitem-id").Wait();
+            testClient.GetFeasibility("test-prj-id", null).Wait();
+            testClient.GetCountries(null).Wait();
+            testClient.GetAttributes("GB", "en", null).Wait();
+            testClient.GetSurveyTopics(null).Wait();
+
+            Assert.Equal<int>(messages.Count, tests.Length);
+            for (int i = 0; i < tests.Length; i++)
+            {
+                Assert.Equal(tests[i], messages[i].RequestUri.PathAndQuery);
+            }
+        }
+
+        [Fact]
+        public void TestQueryString()
+        {
+            HttpRequestMessage message = new HttpRequestMessage();
+            var optOne = new QueryOptions();
+            var optTwo = new QueryOptions();
+            var optThree = new QueryOptions();
+            optOne.AddFilter("title", "Samplify Client Test");
+            optOne.AddFilter("state", StateConstants.Provisioned);
+            optTwo.AddSort("createdAt", SortDirection.Asc);
+            optTwo.AddSort("extProjectId", SortDirection.Desc);
+            optThree.AddFilter("title", "Samplify Client Test");
+            optThree.AddFilter("state", StateConstants.Provisioned);
+            optThree.AddSort("createdAt", SortDirection.Asc);
+            optThree.AddSort("extProjectId", SortDirection.Desc);
+
+            var tests = new Dictionary<QueryOptions, string>();
+            tests.Add(optOne, "/projects?title=Samplify+Client+Test&amp;state=PROVISIONED");
+            tests.Add(optTwo, "/projects?sort=createdAt:asc,extProjectId:desc");
+            tests.Add(optThree, "/projects?title=Samplify+Client+Test&amp;state=PROVISIONED&amp;sort=createdAt:asc,extProjectId:desc");
+
+            SamplifyClient testClient = new MockSamplifyClient((request) =>
+            {
+                message = request;
+            });
+
+            foreach (var t in tests)
+            {
+                testClient.GetAllProjects(t.Key).Wait();
+                Assert.NotNull(message);
+                Assert.Equal(message.RequestUri.PathAndQuery, t.Value);
+            }
+        }
+    }
+}
